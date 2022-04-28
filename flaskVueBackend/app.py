d73b212db39a359cf1c7c53b42eaeb5ab0b9a4db
BingHawk <93518779+BingHawk@users.noreply.github.com>
2022-04-28 11:33:23 +0200

    CORS added

--- flaskVueBackend/app.py
+++ flaskVueBackend/app.py
@@ -1,12 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config["APPLICATION_ROOT"] = "/"
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-
-@app.route('/API/locations', methods=["GET"])
+@app.route('/api/locations', methods=["GET"])
 def get_locations():
     # the endpoint of meassuring stations in italy
     italyEndpoint = "https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/locations?limit=10000&page=1&offset=0&sort=desc&radius=10000&country_id=it&order_by=lastUpdated&dumpRaw=false"
