38c957d912c85a43d4f3bdca193e4914ad5ef7b0
BingHawk <93518779+BingHawk@users.noreply.github.com>
2022-04-28 15:41:12 +0200

    Switch to default icons

--- flaskVueFrontend/components/mapComponent.vue
+++ flaskVueFrontend/components/mapComponent.vue
@@ -2,11 +2,17 @@
   <div id="map-wrap" style="height: 100vh">
     <no-ssr>
       <l-map :zoom="8" :center="[45.4, 9.18]" :attribution="attribution">
-        <marker-icon
+        <L-marker
+          v-for="location in locations"
+          :key="location.id"
+          :lat-lng="location.coordinates"
+        >
+        </L-marker>
+        <!-- <marker-icon
           v-for="location in locations"
           :key="location.id"
           :location="location"
-        ></marker-icon>
+        ></marker-icon> -->
         <l-tile-layer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         ></l-tile-layer>
@@ -16,28 +22,38 @@
 </template>
 
 <script>
-//  import L from 'leaflet';
-import { LMap, LTileLayer } from 'vue2-leaflet'
-import MarkerIcon from './MarkerIcon.vue'
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
+// import MarkerIcon from './MarkerIcon.vue'
+import 'leaflet/dist/leaflet.css'
+import { Icon } from 'leaflet'
+
+delete Icon.Default.prototype._getIconUrl
+
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+})
 
 export default {
   name: 'MapComponent',
   components: {
     LMap,
     LTileLayer,
-    MarkerIcon,
-  },
-  data() {
-    return {
-      attribution:
-        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-    }
+    LMarker,
+    // MarkerIcon,
   },
   props: {
     locations: {
       type: Array,
       required: true,
     },
+  },
+  data() {
+    return {
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    }
   },
 
   head() {
