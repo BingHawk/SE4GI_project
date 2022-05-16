7c2ff6337c50827fd400cef3c9a2e37cd39d3d2a
AhmedAbdalgader <ahmedabdalgader.88@gmail.com>
2022-05-16 19:59:45 +0200

    Update app.py
    End Points: City, Latest value with different parameters.

--- flaskVueBackend/app.py
+++ flaskVueBackend/app.py
@@ -12,22 +12,53 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 def get_locations():
     # the endpoint of meassuring stations in italy
     italyEndpoint = "https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/locations?limit=10000&page=1&offset=0&sort=desc&radius=10000&country_id=it&order_by=lastUpdated&dumpRaw=false"
-    
     # Get the stations
     r = requests.get(italyEndpoint)
 
     locations = []
     print(r.json()['results'][0])
-    for result in r.json()['results']:
-        try:
-            location = {'id': result['id'], 'coordinates': {'lat': result['coordinates']['latitude'], 'lng': result['coordinates']['longitude']} }
-        except KeyError:
-            continue
-        locations.append(location)
-    
-    response = json.dumps({'locations': locations})
+    return r.json()
+    # for result in r.json()['results']:
+    #     try:
+    #         location = {'id': result['id'], 'coordinates': {'lat': result['coordinates']['latitude'], 'lng': result['coordinates']['longitude']} }
+    #     except KeyError:
+    #         continue
+    #     locations.append(location)
+    # response = json.dumps({'locations': locations})
+    # return response
 
-    return response
+@app.route('/api/cities/<city>', methods=["GET"])
+def get_cities(city):
+    cityEndpoint =f"https://docs.openaq.org/v2/locations?limit=15&page=1&offset=0&sort=desc&radius=1000&country_id=IT&city={city}&order_by=lastUpdated&dumpRaw=false"
+    # Get the stations
+    r = requests.get(cityEndpoint)
+    print (f"{city}")
+    return r.json()
 
+@app.route('/api/latests/<location_id>', methods=["GET"])
+def get_latests(location_id):
+    stationEndpoint =f"https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/latest/{location_id}?limit=100&page=1&offset=0&sort=desc&radius=1000&country=IT&order_by=lastUpdated&dumpRaw=false"
+    # Get the stations
+    r = requests.get(stationEndpoint)
+    print (f"{location_id}")
+    return r.json()
+
+# If I am able to return the last updated date and relative parameter of each value.
+    # print(r.json()['results'][0])
+    # stations= []
+    # param = []
+    # lastUp = []
+    # for result in r.json()['results']:
+    #     for m in result['measurements']:
+    #         # param.append(m['parameter'])
+    #         try:
+    #             station = {{'lastUpdated': m['lastUpdated'], 'parameter': m['parameter']}}
+    #         except KeyError:
+    #             continue
+    #         lastUp.append('lastUpdated')
+    #     return (r.json(),lastUp)
+   
 if __name__ == "__main__":
-    print(get_locations())
+    app.run(debug=True,  use_reloader=False)
+    # print(get_cities("Milano"))
+    
