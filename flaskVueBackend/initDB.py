eda04add50bef6cb335cc51ca9f1856f686c5e54
BingHawk <93518779+BingHawk@users.noreply.github.com>
2022-05-30 11:07:21 +0200

    Added all tables
    Made into class for easy import

--- flaskVueBackend/initDB.py
+++ flaskVueBackend/initDB.py
@@ -1,32 +1,62 @@
 import psycopg2
 
-MYUSER = 'postgres'
-MYPWRD = ''
-MYPORT = '5432'
+class Pg:
 
+    # Configuration information. Adjust so it matches you postgres installation before running on you computer. 
+    MYUSER = 'postgres'
+    MYPWRD = 'qrC85Ba9Dpg'
+    MYPORT = '5432'
 
-#create the database table
-def initCities():
-    conn = psycopg2.connect(
-   database="SE4G", user= MYUSER, password= MYPWRD, host='localhost', port= MYPORT
-)
-    #Creating a cursor object using the cursor() method
-    cursor = conn.cursor()
+    # list of the table names
+    tables = ["cities","users","contacts"]
 
-    #Doping EMPLOYEE table if already exists.
-    cursor.execute("DROP TABLE IF EXISTS CITY")
+    # Dictionary of the create statements for each table.
+    #   There has to be one key equal to every item int tables. 
+    #   The name of the created table must be the same as the name of the table in the tables list 
+    create = {
+        "cities": '''CREATE TABLE cities(
+            city_id SMALLINT GENERATED ALWAYS AS IDENTITY
+            city_name CHAR(20) NOT NULL,
+            longitude FLOAT,
+            latitude FLOAT
+        )''',
+        "users": '''CREATE TABLE users(
+            user_id SMALLINT GENERATED ALWAYS AS IDENTITY
+            user_name CHAR(20) NOT NULL
+            user_password VARCHAR NOT NULL
+        ''',
+        "contacts": '''CREATE TABLE users(
+            contact_id SMALLINT GENERATED ALWAYS AS IDENTITY
+            first_name CHAR(20) NOT NULL
+            last_name CHAR(20) NOT NULL
+            description TEXT
+            nationality CHAR(3)
+            email VARCHAR
+        '''
+    }
 
-    #Creating table as per requirement
-    createTable ='''CREATE TABLE CITY(
-    CITY_NAME CHAR(20) NOT NULL,
-    NORTH FLOAT,
-    EAST FLOAT
-    )'''
-    cursor.execute(createTable)
-    #print("Table created successfully........")
-    conn.commit()
-    #Closing the connection
-    conn.close()
+    # helper function to clean up.
+    def _drop(self, table):
+        return "DROP TABLE IF EXISTS {}".format(table)
+
+    #create the database table
+    def __init__(self):
+        conn = psycopg2.connect(
+    database="SE4G", user= self.MYUSER, password= self.MYPWRD, host='localhost', port= self.MYPORT
+    )
+        # Creating a cursor object using the cursor() method
+        cursor = conn.cursor()
+
+        # Creates all the tables in the list of tables. 
+        for table in self.tables:
+            #Doping table if already exists.
+            cursor.execute(self._drop(table))
+
+            cursor.execute(self.create['table'])
+        # print("Table created successfully........")
+            conn.commit()
+        # Closing the connection
+        conn.close()
 
 if __name__ == "__main__":
-    initCities()
+    Pg = Pg()
