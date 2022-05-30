046904b9a4f2b1da96e439511ffccd319cb48773
BingHawk <93518779+BingHawk@users.noreply.github.com>
2022-05-30 11:23:54 +0200

    FIX syntax

--- flaskVueBackend/initDB.py
+++ flaskVueBackend/initDB.py
@@ -12,36 +12,42 @@ class Pg:
 
     # Configuration information. Adjust so it matches you postgres installation before running on you computer. 
     MYUSER = 'postgres'
-    MYPWRD = 'qrC85Ba9Dpg'
-    MYPORT = '5432'
+    MYPWRD = '123456'
+    MYPORT = '5433'
 
     # list of the table names
-    tables = ["cities","users","contacts"]
+    tables = ["city","users","contacts"]
 
     # Dictionary of the create statements for each table.
     #   There has to be one key equal to every item int tables. 
     #   The name of the created table must be the same as the name of the table in the tables list 
     create = {
-        "cities": '''CREATE TABLE cities(
-            city_id SMALLINT GENERATED ALWAYS AS IDENTITY
+        "city": '''CREATE TABLE city(
+            city_id SMALLINT GENERATED ALWAYS AS IDENTITY,
             city_name CHAR(20) NOT NULL,
             longitude FLOAT,
             latitude FLOAT
         )''',
         "users": '''CREATE TABLE users(
-            user_id SMALLINT GENERATED ALWAYS AS IDENTITY
-            user_name CHAR(20) NOT NULL
+            user_id SMALLINT GENERATED ALWAYS AS IDENTITY,
+            user_name CHAR(20) NOT NULL,
             user_password VARCHAR NOT NULL
-        ''',
-        "contacts": '''CREATE TABLE users(
-            contact_id SMALLINT GENERATED ALWAYS AS IDENTITY
-            first_name CHAR(20) NOT NULL
-            last_name CHAR(20) NOT NULL
-            description TEXT
-            nationality CHAR(3)
+        )''',
+        "contacts": '''CREATE TABLE contacts(
+            contact_id SMALLINT GENERATED ALWAYS AS IDENTITY,
+            first_name CHAR(20) NOT NULL,
+            last_name CHAR(20) NOT NULL,
+            description TEXT,
+            nationality CHAR(3),
             email VARCHAR
-        '''
+        )'''
     }
+
+    createTable ='''CREATE TABLE CITY(
+    CITY_NAME CHAR(20) NOT NULL,
+    NORTH FLOAT,
+    EAST FLOAT
+    )'''
 
     # helper function to clean up.
     def _drop(self, table):
@@ -68,8 +74,8 @@ class Pg:
             #Doping table if already exists.
             cursor.execute(self._drop(table))
 
-            cursor.execute(self.create['table'])
-        # print("Table created successfully........")
+            cursor.execute(self.create[table])
+            print("Table created successfully........")
             conn.commit()
         # Closing the connection
         conn.close()
