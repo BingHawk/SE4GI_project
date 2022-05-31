6ddbf4ab790b0a5e0ffd67d7f954aeee9648e10f
BingHawk <93518779+BingHawk@users.noreply.github.com>
2022-05-31 08:49:59 +0200

    Fest getCities
    Gets the coordinates of the cities of "cityDict.keys()" from the database.

--- flaskVueBackend/app.py
+++ flaskVueBackend/app.py
@@ -4,16 +4,57 @@ import psycopg2
 import requests
 import json
 
+MYUSER = 'postgres'
+MYPWRD = '123456'
+MYPORT = '5433'
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config["APPLICATION_ROOT"] = "/"
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
+
 @app.before_first_request
 def initAPI():
+    print("starting setup")
+
     global cityResponse, cityDict
     cityResponse, cityDict = get_cityNames()
+    
+    print("setup complete")
+
+# cityDict keys: ['Alessandria', 'Alfonsine', 'Ancona', 'Arezzo', 'Ascoli Piceno', 'Asti', 'Avellino', 'Bari', 'Barletta-Andria-Trani', 'Belluno', 'Benevento', 'Bergamo', 'Biella', 'Bologna', 'Bolzano/Bozen', 'Brescia', 'Brindisi', 'Cagliari', 'Campobasso', 'Carbonia-Iglesias', 'Carpi', 'Caserta', 'Catanzaro', 'Cento', 'Cesena', 'Chiesanuova', 'Civitavecchia', 'Colorno', 'Como', 'Cosenza', 'Cremona', 'Crotone', 'Cuneo', 'Faenza', 'Ferrara', 'Fiorano Modenese', 'Firenze', 'Foggia', "Forli'", "Forli'-Cesena", 'Frosinone', 'Genova', 'Grosseto', 'Guastalla', 'Imola', 'Imperia', 'Jolanda Di Savoia', 'Langhirano', "L'Aquila", 'La Spezia', 'Latina', 'Lecce', 'Lecco', 'Livorno', 'Lodi', 'Lucca', "Lugagnano Val D'Arda", 'Macerata', 'Mantova', 'Massa-Carrara', 'Matera', 'Mezzani', 'Milano', 'Mirandola', 'Modena', 'Molinella', 'Monza E Della Brianza', 'Napoli', 'Novara', 'Nuoro', 'Olbia-Tempio', 'Oristano', 'Ostellato', 'Padova', 'Parma', 'Pavia', 'Perugia', 'Pesaro E Urbino', 'Pescara', 'Piacenza', 'Pisa', 'Pistoia', 'Porretta Terme', 'Potenza', 'Prato', 'Ravenna', 'Reggio Di Calabria', "Reggio Nell'Emilia", 'Rieti', 'Rimini', 'Roma', 'Rovigo', 'Salerno', 'San Clemente', 'San Lazzaro Di Savena', 'San Leo', 'Sassari', 'Sassuolo', 'Savignano Sul Rubicone', 'Savona', 'Siena', 'Sogliano Al Rubicone', 'Sondrio', 'Sorbolo', 'Taranto', 'Teramo', 'Terni', 'Torino', 'Trento', 'Treviso', 'Varese', 'Venezia', 'Verbano-Cusio-Ossola', 'Vercelli', 'Verona', 'Verucchio', 'Vibo Valentia', 'Vicenza', 'Villa Minozzo', 'Viterbo']
+def getCityCoords():
+
+    cityString = ""
+    for city in cityDict.keys():
+        city = city.replace("'","''")
+        cityString += "'{}',".format(city)
+    cityString += cityString[0:-1]
+
+    query = "SELECT * FROM city WHERE city_name in ({});".format(cityString)
+
+    conn = psycopg2.connect(
+    database="SE4G", user = MYUSER, password= MYPWRD, host='localhost', port= MYPORT
+    )
+
+    c = conn.cursor()
+    c.execute(query)
+    res = c.fetchall()
+    conn.commit()
+    conn.close()
+
+    # Format: cityCoords['city_name'] = [longitude, latitude]
+    # The format can be changed through the loop below
+    cityCoords = {} 
+    for result in res:
+        cityCoords[result[1]] = [result[2],result[3]]
+    
+    return cityCoords
+
+
+
 
 
     
@@ -42,7 +83,6 @@ def get_locations():
 #EndPoint for the cities
 @app.route('/api/cities') # Moved the actual API call to another function to be able to reuse get_cityNames() elsewhere. 
 def serve_cityNames():
-    # response, cityDict = get_cityNames()
     return cityResponse
 
 def get_cityNames():
@@ -66,11 +106,10 @@ def get_cityNames():
             continue
         if cityname not in citiesname: #Avoiding adding duplicates
             citiesname.append(cityname)
-    print(citiesname)
     response = json.dumps({'cities': citiesname})
     return response, cityDict
 
 if __name__ == "__main__":
     app.run(debug=True,  use_reloader=False)
-    get_cityNames()
+    getCityCoords()
     # print(get_locations())
