e503ba603e683725900d0b984a4ea7d3f8c4fb67
AlbaLunner <101339191+AlbaLunner@users.noreply.github.com>
2022-06-03 12:23:07 +0200

    Delete get_latest2.py

--- flaskVueBackend/get_latest2.py
+++
@@ -1,3 +0,0 @@
-for city in getCityCoords().keys():
-            coords = getCityCoords()
-            locations[city] = {'cityName': city, 'coordinates': coords[city], 'particles': result['measurements']}
