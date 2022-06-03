c0bae586b8c951eec5b616fb93cc8cc09065db8e
BingHawk <93518779+BingHawk@users.noreply.github.com>
2022-06-03 14:15:22 +0200

    Added a faster enpoint for months

--- flaskVueBackend/historical.py
+++ flaskVueBackend/historical.py
@@ -2,6 +2,8 @@ from decimal import DivisionByZero
 from flask import jsonify
 import requests
 import datetime as dt
+import concurrent.futures
+
 
 # t = dt.datetime.now()
 # now = t.replace(second = 0, minute = 0)
@@ -13,6 +15,55 @@ import datetime as dt
 # print(oneMonthAgo.strftime(dateformat)+"+00:00")
 # print(now.strftime(dateformat)+"+00:00")
 
+def getMonthDataF(city):
+    days = 30
+    print("querying moth data for {}".format(city))
+    t = dt.datetime.now()
+    startDay = t.replace(second = 59, minute = 59, hour=23) # end of today the starting day of query
+    # endDay = startDay - dt.timedelta(days = days) #iterate untill d0 == stoptime
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for _ in range(days):
+            endDay = startDay - dt.timedelta(days = 1)
+            f = executor.submit(queryByDay, startDay, endDay, city)
+            futures.append(f)
+            startDay = endDay
+        res = {}
+        paramUnits = {}
+        for future in concurrent.futures.as_completed(futures):
+
+            resDay, paramUnitsDay = future.result()
+            res.update(resDay)
+            paramUnits.update(paramUnitsDay)
+
+    # Desired JSON scheme
+    #  {
+    # "time_month": {
+    #     particle1: {
+    #         "data": [day1, day2, ... day30],
+    #         "unit":unit
+    #         },
+    #     particle2: {
+    #         "data": [day1, day2, ... day30],
+    #         "unit": unit
+    #         }
+    # }
+        time_month = {}
+        for param in paramUnits.keys():
+            time_month[param] = {"data": [], "unit": paramUnits[param]}
+
+            currentDay = endDay.replace(hour=0,minute=0,second=0,microsecond=0)# + dt.timedelta(days = 1)
+            while currentDay != endDay.replace(hour=0,second=0,minute=0,microsecond=0) + dt.timedelta(days = days):
+                currentDay = currentDay + dt.timedelta(days = 1)
+                try:
+                    avg = round(res[currentDay][param]['sum']/res[currentDay][param]['n'],4)
+                    time_month[param]['data'].append(avg)
+                except KeyError:
+                    print("missing day: {} or parameter: {}".format(currentDay, param), end=("\r"))
+                    time_month[param]['data'].append(None)
+        
+        return {"time_month":time_month}
 
 def getMonthData(city):
     print("querying moth data for {}".format(city))
@@ -120,41 +171,37 @@ def getYearData(city):
 
 def queryByDay(startDay, endDay, city):
     inFormat = "%Y-%m-%dT%H%%3A%M%%3A%S" # Format to build query string
-    summerFormat = "%Y-%m-%dT%H:%M:%S+02:00" # Format recieved from openAQ 
-    winterFormat = "%Y-%m-%dT%H:%M:%S+01:00" # Format recieved from openAQ 
-
+    utcFormat = "%Y-%m-%dT%H:%M:%S+00:00" # Format recieved from openAQ 
 
     d0 = startDay
     d1 = d0 - dt.timedelta(days = 1) # The day before 
-    limit = 1000 # Number of results accepted per query. Fewer means better preformance but risk of missing data. 
-    maxlimit = 5000 # The max limit the program is allowed to raise to. 5000 always works, 10'000 sometimes fails
+    limit = 5000 # Number of results accepted per query. Fewer means better preformance but risk of missing data. 
 
     iter = 0
     paramUnits= {}
     res = {}
+
+    maxPages = 10
     while d0 != endDay: # Looping until every day of the month is done
         print("Querying day {}".format(iter+1),end="\r")
         page = 1
-        while True: #Looping though paginations
+        getNextPage = True
+        while getNextPage: #Looping though paginations
             d0String = d0.strftime(inFormat)+"%2B00%3A00"
             d1String = d1.strftime(inFormat)+"%2B00%3A00"
             url = f"https://api.openaq.org/v2/measurements?date_from={d1String}&date_to={d0String}&limit={limit}&page={page}&offset=0&sort=desc&radius=1000&city={city}&order_by=datetime"
-            page = page+1
+            # print(url)
 
             r = requests.get(url)
             resJson = r.json()
+            if resJson['meta']['found'] < limit or page == maxPages:
+                getNextPage = False
+            else:
+                if page == 1:
+                    maxPages = resJson['meta']['found']// limit + 1
+                    print(maxPages)
 
-            # informing about limit issues if present.
-            if resJson['meta']['found'] > limit and limit < maxlimit:
-                print("WARNING: The limit is lower than number of results, some results are lost")
-                print("\tLimit: {}, Number of results: {}".format(limit, resJson['meta']['found']))
-                if resJson['meta']['found'] < maxlimit:
-                    print("\tRasing limit to {}".format(resJson['meta']['found']))
-                    limit = resJson['meta']['found']
-                else:
-                    print("\tRasing limit to {}".format(maxlimit))
-                    print("\tCannot raise limit more, at maxlimit")
-                    limit = maxlimit
+            page = page+1
 
             # When an empty page is recieved, goes on to next day. 
             if not resJson['results']:
@@ -175,11 +222,7 @@ def queryByDay(startDay, endDay, city):
             #    }
             # }
             for result in resJson['results']:
-                try:
-                    day = dt.datetime.strptime(result['date']['local'],summerFormat).replace(hour=0,minute=0,second=0)
-                except ValueError:
-                    day = dt.datetime.strptime(result['date']['local'],winterFormat).replace(hour=0,minute=0,second=0)
-
+                day = dt.datetime.strptime(result['date']['utc'],utcFormat).replace(hour=0,minute=0,second=0)
 
                 if day not in res.keys():
                     res[day] = {}
@@ -206,10 +249,11 @@ def queryByDay(startDay, endDay, city):
         
     return res, paramUnits
 
-
 if __name__ == "__main__":
     cities = ['Roma', 'MILANO', 'Firenze']
-    resMonth = getMonthData('MILANO')
+    resMonth = getMonthData('Roma')
+    resMonth = getMonthDataF('Roma')
+
     print(resMonth)
     # resYear = getYearData('Firenze')
     # print(resYear)
