9dcb4c8099d1f030e6964d98d532ada67ae8d591
BingHawk <93518779+BingHawk@users.noreply.github.com>
2022-06-04 22:33:25 +0200

    removed a misstaken comment

--- flaskVueBackend/app.py
+++ flaskVueBackend/app.py
@@ -352,7 +352,7 @@ def login():
         response = cur.fetchall() #guess
         return jsonify(response) #render_template('auth/login.html')
 
-@app.route('/logout') # Logout will not need any endpoint, it will be handled 100% on frontend /Leo
+@app.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('register'))
