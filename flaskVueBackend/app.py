ae7af452e67ade74f98a0964d95fb9941708c40a
BingHawk <93518779+BingHawk@users.noreply.github.com>
2022-06-06 13:47:11 +0200

    Update app.py

--- flaskVueBackend/app.py
+++ flaskVueBackend/app.py
@@ -7,8 +7,8 @@ from historical import getMonthData, getYearData
 
 #### GLOBAL VARIABLES
 MYUSER = 'postgres'
-MYPWRD = 'qrC85Ba9Dpg'
-MYPORT = '5432'
+MYPWRD = '123456'
+MYPORT = '5433'
 
 #### FLASK CONFIGURATION
 app = Flask(__name__)
