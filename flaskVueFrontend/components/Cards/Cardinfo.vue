d6d9b10d3b5c7dc55084e21631641a9af04720fb
BingHawk <93518779+BingHawk@users.noreply.github.com>
2022-06-07 21:44:18 +0200

    Contacts page styling

--- flaskVueFrontend/components/Cards/Cardinfo.vue
+++ flaskVueFrontend/components/Cards/Cardinfo.vue
@@ -1,43 +1,28 @@
-<template >
-    <div >
-        <card style="margin: 5px;">
-            <div >
-                <v-col cols="5">
-                    <v-container class="my-5" >
-                        <v-layout row wrap>
-                            <v-flex sm2 md3 lg5 >
-                                <v-card flat class="text-xs-center ma-3">
-                                    <v-responsive class="pt-4"  v-for="person in persons" :key="person['last_name']"> 
-                                        <v-card-text > 
-                                        <div class="subheading">{{ person.first_name }}</div>
-                                        <div class="grey--text">{{ person['last_name'] }}</div>
-                                        <div class="grey--text">{{ person.nationality }}</div>
-                                        <div class="grey--text">{{ person.description }}</div>
-                                        <div  > <a class="tim-icons icon-email-85"  :href="'mailto:'+ person.email"></a> </div>
-                                    </v-card-text>
-                                        <!-- <v-avatar size="100" class="grey lighten-2">  
-                                            <img :src="person.avatar">
-                                        </v-avatar> -->
-                                    </v-responsive>
-                                </v-card>
-                            </v-flex>
-                        </v-layout>
-                </v-container>
-                </v-col>
-            </div>
-        </card>
-
-           
-            
-
-    </div>
-    
+<template>
+  <div>
+    <card v-for="person in persons" :key="person.last_name">
+      <div slot="header">
+        {{ person.first_name }} {{ person.last_name }}
+        <p class="text-muted">Nationality: {{ person.nationality }}</p>
+        <div style="border-bottom: 1px solid #2b3553"></div>
+      </div>
+      <div>
+        <p class="text">{{ person.description }}</p>
+      </div>
+      <div slot="footer">
+        <a class="tim-icons icon-email-85" :href="'mailto:' + person.email"></a>
+        <a :href="'mailto:' + person.email">: {{
+          person.email
+        }}</a>
+      </div>
+    </card>
+  </div>
 </template>
 
 <script>
 import Card from "./Card.vue";
 export default {
-  components: { Card},
+  components: { Card },
   name: "CardInfo",
   props: { persons: {} },
 };
