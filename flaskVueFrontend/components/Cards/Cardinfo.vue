91ae0fa4cbcbb1f16b790fed907e3c14d6490fb8
AhmedAbdalgader <ahmedabdalgader.88@gmail.com>
2022-06-07 21:23:27 +0200

    Nothing prettier
    Another card component but doesn't change anything.

--- flaskVueFrontend/components/Cards/Cardinfo.vue
+++ flaskVueFrontend/components/Cards/Cardinfo.vue
@@ -1,5 +1,5 @@
 <template >
-    <div >
+  <!-- <div >
         <card style="margin: 5px;">
             <div >
                 <v-col cols="5">
@@ -14,11 +14,11 @@
                                         <div class="grey--text">{{ person.nationality }}</div>
                                         <div class="grey--text">{{ person.description }}</div>
                                         <div  > <a class="tim-icons icon-email-85"  :href="'mailto:'+ person.email"></a> </div>
-                                    </v-card-text>
-                                        <!-- <v-avatar size="100" class="grey lighten-2">  
+                                    </v-card-text> -->
+  <!-- <v-avatar size="100" class="grey lighten-2">  
                                             <img :src="person.avatar">
                                         </v-avatar> -->
-                                    </v-responsive>
+  <!-- </v-responsive>
                                 </v-card>
                             </v-flex>
                         </v-layout>
@@ -26,18 +26,67 @@
                 </v-col>
             </div>
         </card>
-
-           
-            
-
     </div>
+     -->
+
+  <div>
+    <v-card
+      class="mx-auto"
+      max-width="400"
+      v-for="person in persons"
+      :key="person['last_name']"
+    >
+      <v-img
+        class="white--text align-end"
+        height="200px"
+        src="https://media.istockphoto.com/photos/akihabara-tokyo-picture-id484915982?k=20&m=484915982&s=612x612&w=0&h=jeEPg-Xt5Uv-vUoeTVpaW0ilaGJ1LuazPU-7dUBnhUs="
+      >
+        <v-card-title>{{ person.first_name }}</v-card-title>
+      </v-img>
+
+      <v-card-subtitle class="pb-0">
+        {{ person["last_name"] }}
+      </v-card-subtitle>
+
+      <v-card-text class="text--primary">
+        <div>{{ person.nationality }}</div>
+
+        <div>{{ person.description }}</div>
+      </v-card-text>
+
+      <v-card-actions>
+        <div>
+          <a
+            class="tim-icons icon-email-85"
+            :href="'mailto:' + person.email"
+          ></a>
+        </div>
+        
+        <!-- <v-btn
+         color="orange"
+         text
+       >
+         Share
+       </v-btn>
+
+       <v-btn
+         color="orange"
+         text
+       >
+         Explore
+       </v-btn> -->
+        
+      </v-card-actions>
+      
+    </v-card>
     
+  </div>
 </template>
 
 <script>
 import Card from "./Card.vue";
 export default {
-  components: { Card},
+//   components: { Card },
   name: "CardInfo",
   props: { persons: {} },
 };
